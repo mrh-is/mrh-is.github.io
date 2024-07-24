23418e24e3cd5dbd494a36a551838cadc83fca80
Michael Helmbrecht <me@mrh.is>
2024-07-24 11:00:05 -0400

    Move to GH deployment

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import { navLinkForProject, projects } from "$lib/types/Project";
 import type { LayoutLoad } from "./$types";
 
+export const prerender = true;
+
 export const load = (() => {
   return {
     projects: projects.map(navLinkForProject),
