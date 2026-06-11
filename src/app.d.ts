8f7fdb42113994a65dd31045305cbeba8f534dd7
Michael Helmbrecht <me@mrh.is>
2026-06-11 03:34:06 -0400

    chore: remove unused Cloudflare Platform types from app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,16 +2,6 @@ import type { ColorScheme } from "$lib/types/Colors";
 
 declare global {
   namespace App {
-    interface Platform {
-      env: {
-        COUNTER: DurableObjectNamespace;
-      };
-      context: {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        waitUntil(promise: Promise<any>): void;
-      };
-      caches: CacheStorage & { default: Cache };
-    }
     interface PageData {
       title: string;
       description: string;
