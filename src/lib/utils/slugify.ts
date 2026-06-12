63108390053528b6f0c66cc8c0b9b327ee8c2371
Michael Helmbrecht <me@mrh.is>
2026-06-12 15:56:42 -0400

    feat: add slugify utility

---
+++ src/lib/utils/slugify.ts
@@ -0,0 +1,7 @@
+export function slugify(text: string | undefined): string | undefined {
+  if (!text) {return undefined;}
+  return text
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-|-$/g, "");
+}
